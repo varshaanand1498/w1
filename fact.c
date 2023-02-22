a825671d49a84fedfb1fbc811fbd8e2a2a4b6981
varsha <varsha@gmail.com>
2023-02-22 14:52:23 +0000

    modified files

--- fact.c
+++ fact.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int main() {
+factorial() {
   int i, Digit, fact = 1;
   printf("Enter a number to calculate its factorial : ");
   scanf("%d", &Digit);
   for (i = 1; i <= Digit; i++)
     fact = fact * i;
     printf("Factorial of %d = %d\n", Digit, fact);
-    return 0;
+    //return 0;
 }
